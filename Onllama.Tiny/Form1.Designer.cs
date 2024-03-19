d72c2a60d7d8b45e841058c1f8d172296ada23ab
Milkey Tan <me@milione.cc>
2024-03-19 22:33:55 +0800

    添加项目文件。

---
+++ Onllama.Tiny/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Onllama.Tiny
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            table1 = new AntdUI.Table();
+            panel1 = new AntdUI.Panel();
+            button1 = new AntdUI.Button();
+            select1 = new AntdUI.Select();
+            progress1 = new AntdUI.Progress();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // table1
+            // 
+            table1.Dock = DockStyle.Fill;
+            table1.Location = new Point(0, 0);
+            table1.Name = "table1";
+            table1.Size = new Size(622, 382);
+            table1.TabIndex = 0;
+            table1.Text = "table1";
+            table1.CellButtonClick += table1_CellButtonClick;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(select1);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 337);
+            panel1.Name = "panel1";
+            panel1.Shadow = 5;
+            panel1.Size = new Size(622, 45);
+            panel1.TabIndex = 1;
+            panel1.Text = "panel1";
+            // 
+            // button1
+            // 
+            button1.BorderWidth = 1F;
+            button1.Ghost = true;
+            button1.Location = new Point(535, 8);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 30);
+            button1.TabIndex = 1;
+            button1.Text = "下载";
+            button1.Click += button1_Click;
+            // 
+            // select1
+            // 
+            select1.Items.AddRange(new object[] { "qwen:4b", "qwen:7b", "qwen:14b", "yi:6b", "yi:34b", "gemma:7b", "deepseek-llm:7b", "llama2-chinese:7b", "llama2-chinese:13b", "orionstar/orion14b-q4:latest", "roger/minicpm:latest", "onekuma/sakura-13b-lnovel-v0.9b-q2_k" });
+            select1.Location = new Point(8, 8);
+            select1.Name = "select1";
+            select1.SelectedIndex = 0;
+            select1.SelectedValue = "qwen:4b";
+            select1.Size = new Size(521, 30);
+            select1.TabIndex = 0;
+            select1.Text = "qwen:4b";
+            // 
+            // progress1
+            // 
+            progress1.Dock = DockStyle.Top;
+            progress1.Location = new Point(0, 0);
+            progress1.Name = "progress1";
+            progress1.Radius = 1;
+            progress1.Size = new Size(622, 5);
+            progress1.TabIndex = 2;
+            progress1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(622, 382);
+            Controls.Add(progress1);
+            Controls.Add(panel1);
+            Controls.Add(table1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2, 3, 2, 3);
+            Name = "Form1";
+            Text = "Models";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AntdUI.Table table1;
+        private AntdUI.Panel panel1;
+        private AntdUI.Select select1;
+        private AntdUI.Button button1;
+        private AntdUI.Progress progress1;
+    }
+}
---
+++ Onllama.Tiny/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+using AntdUI;
+using OllamaSharp;
+
+namespace Onllama.Tiny
+{
+    public partial class Form1 : AntdUI.BaseForm
+    {
+        public static OllamaApiClient OllamaApi = new OllamaApiClient("http://127.0.0.1:11434");
+
+        public Form1()
+        {
+            InitializeComponent();
+            table1.Columns = new AntdUI.Column[]
+            {
+                new AntdUI.Column("name", "模型名称"),
+                new AntdUI.Column("size", "模型大小"),
+                new AntdUI.Column("modifiedAt", "上次修改"),
+                new AntdUI.Column("families", "系列"),
+                new AntdUI.Column("quantization", "格式与规模"),
+                new AntdUI.Column("btns", "操作") {Fixed = true},
+            };
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                progress1.Hide();
+                Environment.SetEnvironmentVariable("OLLAMA_ORIGINS", "*", EnvironmentVariableTarget.User);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            var modelsClasses = new List<ModelsClass>();
+            var models = Task.Run(async () => await OllamaApi.ListLocalModels()).Result;
+            foreach (var item in models)
+            {
+                var q = new List<CellTag>()
+                {
+                    new(item.Details.Format.ToUpper(),TTypeMini.Default),
+                    new(item.Details.ParameterSize.ToUpper(),TTypeMini.Success),
+                    new(item.Details.QuantizationLevel.ToUpper(),TTypeMini.Warn)
+                };
+                modelsClasses.Add(new ModelsClass()
+                {
+                    name = item.Name,
+                    size = (item.Size / 1024.00 / 1024.00 / 1024.00).ToString("0.00") + "G",
+                    modifiedAt = item.ModifiedAt,
+                    families = item.Details.Families.Select(x => new CellTag(x.ToUpper(), TTypeMini.Info)).ToArray(),
+                    quantization = q.ToArray(),
+                    btns = new AntdUI.CellLink[]
+                    {
+                        new AntdUI.CellButton("chat", "NextChat", AntdUI.TTypeMini.Default)
+                            {Ghost = true, BorderWidth = 1},
+                        new AntdUI.CellButton("delete", "删除", AntdUI.TTypeMini.Error)
+                            {Ghost = true, BorderWidth = 1}
+                    }
+                });
+            }
+            table1.DataSource = modelsClasses;
+        }
+
+        public class ModelsClass : AntdUI.NotifyProperty
+        {
+            string _name;
+            public string name
+            {
+                get => _name;
+                set
+                {
+                    _name = value;
+                    OnPropertyChanged("name");
+                }
+            }
+
+            string _size;
+            public string size
+            {
+                get => _size;
+                set
+                {
+                    _size = value;
+                    OnPropertyChanged("size");
+                }
+            }
+
+            DateTime _modifiedAt;
+            public DateTime modifiedAt
+            {
+                get => _modifiedAt;
+                set
+                {
+                    _modifiedAt = value;
+                    OnPropertyChanged("modifiedAt");
+                }
+            }
+
+            CellTag[]? _families;
+            public CellTag[]? families
+            {
+                get => _families;
+                set
+                {
+                    _families = value;
+                    OnPropertyChanged("families");
+                }
+            }
+
+            CellTag[]? _quantization;
+            public CellTag[]? quantization
+            {
+                get => _quantization;
+                set
+                {
+                    _quantization = value;
+                    OnPropertyChanged("quantization");
+                }
+            }
+
+            CellLink[]? _btns;
+            public CellLink[]? btns
+            {
+                get => _btns;
+                set
+                {
+                    _btns = value;
+                    OnPropertyChanged("btns");
+                }
+            }
+
+        }
+
+        private void table1_CellButtonClick(object sender, CellLink btn, MouseEventArgs args, object record,
+            int rowIndex, int columnIndex)
+        {
+            if (record is ModelsClass data)
+            {
+                if (btn.Id == "delete")
+                {
+                    AntdUI.Modal.open(new AntdUI.Modal.Config(this, "您确定要删除模型吗？",
+                        new[]
+                        {
+                            new Modal.TextLine(data.name, Style.Db.Primary),
+                            new Modal.TextLine(data.size, Style.Db.TextSecondary)
+                        }, TType.Warn)
+                    {
+                        OkType = TTypeMini.Error,
+                        OkText = "删除",
+                        OnOk = _ =>
+                        {
+                            Task.Run(async () => await OllamaApi.DeleteModel(data.name)).Wait();
+                            return true;
+                        }
+                    });
+                }
+                else if (btn.Id == "chat")
+                {
+                    Process.Start(new ProcessStartInfo($"https://app.nextchat.dev/#/?settings={{%22url%22:%22http://127.0.0.1:11434%22}}") { UseShellExecute = true });
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AntdUI.Modal.open(new AntdUI.Modal.Config(this, "您确定要下载模型吗？",
+                new[]
+                {
+                    new Modal.TextLine(select1.Text, Style.Db.Primary)
+                }, TType.Success)
+            {
+                OkType = TTypeMini.Success,
+                OkText = "下载",
+                OnOk = _ =>
+                {
+                    Invoke(progress1.Show);
+                    Task.Run(() => OllamaApi.PullModel(select1.Text, (x) =>
+                    {
+                        Invoke(() =>
+                        {
+                            progress1.Value = (float) x.Completed / x.Total;
+                            Text = x.Status + ":" + x.Digest;
+                            if (x.Status == "success" || string.IsNullOrEmpty(x.Status)) progress1.Hide();
+                        });
+                    }));
+                    return true;
+                }
+            });
+        }
+    }
+}
